eb67e93bb2250498d824df0a5c1067a4ae31e1c2
Rudolf Hoehler <rudolfmdlt@gmail.com>
2012-03-28 15:11:51 +0200

    Updated the folder structure and moved our(Graham and Rudolf) code into the "correct" folder. I hope the case sensitive folder strucutre doesn't annoy?

---
+++ src/za/ac/wits/elen4010/fluidsim/mpiNodalCode/Test.java
@@ -0,0 +1,46 @@
+
+class Test {
+    
+    public static void main(String[] args) {
+	    
+	    final int rows = 8, cols = 8;
+		
+	    // "Read in" Data. The Data array can be rectangular
+	    int dataArray[][] = new int[rows][cols];
+    	
+	    for (int i=0; i != rows; i++)
+	        for (int j=0; j != cols; j++)
+	            dataArray[i][j] = 3;
+    	
+	    Data data = new Data(dataArray);	
+    	
+	    // Display data
+	    Test.displayArray("Initial 2D array :", data);
+    	
+	    // Split up data between 4 processes
+	    int processes = 4;
+	    Data dataObjectArray[] = new Data[processes];
+	    dataObjectArray = arrayManipulator.splitData(data, processes);
+    	
+	    // Display Data sets
+	    for(int i = 0; i != processes; i++)
+	    {
+	        System.out.println("Initial dataset values for rank " + dataObjectArray[i].getRank());
+	        Test.displayArray("Data set value: ", dataObjectArray[i]);
+	    }
+    	
+	    // Aggregate Data
+	    Data aggregatedData = arrayManipulator.aggregateData(dataObjectArray);
+    	
+	    // Display aggregated data
+	    Test.displayArray("Aggregated 2D array :", aggregatedData);
+    	
+	}
+    
+    public static void displayArray(String message, Data data)
+    {
+        for (int i=0; i != data.getYLength(); i++)
+            for (int j=0; j != data.getXLength(); j++)
+                System.out.println(message + data.getDataArray()[i][j] );
+    }
+}
