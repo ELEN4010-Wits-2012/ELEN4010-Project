8ac3bb7c21b35f18812f0c0df4feabb2f990dfaa
Edward John Steere <edward.steere@gmail.com>
2012-05-04 14:00:14 +0200

    Haven't been able to test them yet because I don't have the MPI Java environment up and running, but they should work. 'They' are the new IO classes, so that we can 'inject' fake IO objects for testing purposes

---
+++ src/za/ac/wits/elen4010/fluidsim/mpiNodalCode/MpiIO.java
@@ -0,0 +1,52 @@
+// MpiIO.java
+
+/**
+ * Interface for an MPI send and receive object which should be implemented by any object which perf-
+ * orms send/receive operations or fakes them
+ * @author Edward Steere
+ * @see TrueIO
+ * @see FakedIO
+ */
+public interface MpiIO
+{
+
+    /**
+     * Acts as the sending command for the class which implements the interface
+     * @param data
+     *             The data which will be sent over the network in the format of a buffer of the ite-
+     *             ms to be sent
+     * @param offset
+     *             The off set from the start of the buffer to begin sending from
+     * @param count
+     *             The number of items (starting at offset) to send
+     * @param dataType
+     *             An indicator for the datatype which was sent (i.e. the type found in the buffer)
+     * @param destination
+     *             The rank which should receive the command
+     * @param tag
+     *             The tag which defines the user specified message type
+     */
+    public void mpiSend( Object data, int offset, int count, Datatype dataType, int destination, int tag );
+
+
+    /**
+     * Acts as the receiving command for the class which implements the interface
+     * @param data
+     *             The data which will be sent over the network in the format of a buffer of the ite-
+     *             ms to be sent
+     * @param offset
+     *             The off set from the start of the buffer to begin sending from
+     * @param count
+     *             The number of items (starting at offset) to send
+     * @param dataType
+     *             An indicator for the datatype which was sent (i.e. the type found in the buffer)
+     * @param source
+     *             The rank which sent the message
+     * @param tag
+     *             The tag which defines the user specified message type
+     * @return The status of the operation which contains important information such as the source a-
+     * nd tag fields
+     */
+    public Status mpiReceive( object data, int offset, int count, Datatype dataType, int source, int tag );
+
+}
---
+++ src/za/ac/wits/elen4010/fluidsim/mpiNodalCode/TrueIO.java
@@ -0,0 +1,73 @@
+// TrueIO.java
+
+// Standard dependancies
+import mpi.*;
+
+/**
+ * A class which implements {@link MpiIO MpiIO} to achieve the "true" vesion of the MPI IO operations
+ * @author Edward Steere
+ * @see MpiIO
+ * @see FakedIO
+ */
+public class TrueIO implements MpiIO
+{
+
+    // ===Public Methods===
+
+    /**
+     * Creates a new TrueIO object (since it has no state there are no initialisation conditions
+     */
+    public TrueIO()
+    {}
+
+    /**
+     * Implementation of the {@link MpiIO MpiIO} {@link mpiSend mpiSend} function which uses the MPI-
+     * Java commands
+     * @param data
+     *             The data which will be sent over the network in the format of a buffer of the ite-
+     *             ms to be sent
+     * @param offset
+     *             The off set from the start of the buffer to begin sending from
+     * @param count
+     *             The number of items (starting at offset) to send
+     * @param dataType
+     *             An indicator for the datatype which was sent (i.e. the type found in the buffer)
+     * @param destination
+     *             The rank which should receive the command
+     * @param tag
+     *             The tag which defines the user specified message type
+     */
+    public void mpiSend( Object data, int offset, int count, Datatype dataType, int destination, int tag )
+    {
+
+        MPI.COMM_WORLD.Send( data, offset, count, dataType, destination, tag );
+
+    }
+
+    /**
+     * Implementation of the {@link MpiIO MpiIO} {@link mpiReceive mpiReceive} function which uses the
+     * MPI-Java commands
+     * @param data
+     *             The data which will be sent over the network in the format of a buffer of the ite-
+     *             ms to be sent
+     * @param offset
+     *             The off set from the start of the buffer to begin sending from
+     * @param count
+     *             The number of items (starting at offset) to send
+     * @param dataType
+     *             An indicator for the datatype which was sent (i.e. the type found in the buffer)
+     * @param source
+     *             The rank which sent the message
+     * @param tag
+     *             The tag which defines the user specified message type
+     * @return The status of the operation which contains important information such as the source a-
+     * nd tag fields
+     */
+    public Status mpiReceive( object data, int offset, int count, Datatype dataType, int source, int tag )
+    {
+
+        MPI.COMM_WORLD.Recv( data, offset, count, dataType, source, tag );
+
+    }
+
+}
